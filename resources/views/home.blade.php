53e1f0a6c83a52367935e96e6ab8e7721b817468
Ricardo Mota <ricardo.hb@outlook.com>
2025-05-20 00:40:42 +0200

    add correct link to sell now

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
             <div class="col-md-6">
                 <div class="bg-white p-5 rounded shadow" style="max-width: 420px;">
                     <h2 class="fw-bold mb-3">Ready to share your art with the world?</h2>
-                    <a href="#" class="btn artify-btn w-100 mb-3">Sell now</a>
+                    <a href="{{ route('item.new') }}" class="btn artify-btn w-100 mb-3">Sell now</a>
                     <div class="text-center">
                         <a href="#" class="">Learn how it works</a>
                     </div>
