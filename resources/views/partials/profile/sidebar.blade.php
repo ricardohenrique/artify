51c01f6d06881abf7544df323c6aa1ed498c117a
Ricardo Mota <r.benevidesmota@clipmyhorse.tv>
2025-06-04 12:17:13 +0200

    add dashboard to side menu profile

--- resources/views/partials/profile/sidebar.blade.php
+++ resources/views/partials/profile/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-3">
     <div class="list-group shadow-sm">
-        <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
-            Profile summary
+        <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-action" aria-current="true">
+            Dashboard
         </a>
         <a href="{{ route('member.favorites', Auth::id()) }}" class="list-group-item list-group-item-action">Your favorite artworks ({{ $user->favorites_count }})</a>
         <a href="#" class="list-group-item list-group-item-action">Artists you follow ({{ $user->following_count }})</a>
