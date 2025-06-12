02f2219093a8781f52d4e033253985129b5cf580
Ricardo Mota <ricardo.hb@outlook.com>
2025-06-13 00:22:20 +0200

    add logo and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Artify - @yield('title', 'Discover & Sell Art')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="{{ asset('artify_favicon-02.ico') }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,8 @@
         <!-- Top Row -->
         <div class="d-flex flex-wrap align-items-center justify-content-between">
             <a class="navbar-brand d-flex align-items-center me-3" href="/">
-                <img src="{{ asset('artify-logo-opt.png') }}" alt="Artify Logo" class="logo me-2" style="height: 40px;">
+                {{-- <img src="{{ asset('artify-logo-opt.png') }}" alt="Artify Logo" class="logo me-2" style="height: 40px;"> --}}
+                <img src="{{ asset('artify_navbar-04.png') }}" alt="Artify Logo" class="logo me-2" style="height: 40px;">
             </a>
 
             <!-- Search -->
