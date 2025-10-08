84d19b41c04168946fa7369291650c8525d93788
Ricardo Mota <r.benevidesmota@clipmyhorse.tv>
2025-10-08 15:33:02 +0200

    only artists to artists page

--- app/Repositories/ArtistRepository.php
+++ app/Repositories/ArtistRepository.php
@@ -9,6 +9,7 @@ class ArtistRepository
     public function getPaginatedArtistsWithCounts($perPage = 12)
     {
         return User::withCount(['paintings', 'followers'])
+            ->where('user_type_id', '=', 3)
             ->latest()
             ->paginate($perPage);
     }
